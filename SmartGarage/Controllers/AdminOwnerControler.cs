4d15c85fa610246275f926763ebbd2163d62c3e2
Sassho7 <gringo.san5@gmail.com>
2024-01-29 18:58:16 +0200

    Initial clone of repository

---
+++ SmartGarage/Controllers/AdminOwnerControler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartGarage.Controllers
+{
+    public class AdminOwnerControler : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
